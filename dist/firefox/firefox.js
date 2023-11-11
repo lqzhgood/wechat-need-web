e03936e34ba488e356eccfb449e948503a7a980b
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-11-11 22:51:55 +0800

    🔧 build(1.1.1): firefox 严格模式

--- dist/firefox/firefox.js
+++ dist/firefox/firefox.js
@@ -1,6 +1,6 @@
-const url = new URL(window.location.href);
-
+'use strict';
+const url = new URL(window.location);
 if (url.searchParams.get('target') !== 't') {
-	url.searchParams.set('target', 't')
-    window.location.href = url.toString();
+    url.searchParams.set('target', 't');
+    window.location.href = url.href;
 }
