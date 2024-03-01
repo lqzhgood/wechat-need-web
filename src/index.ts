b1bddaa8cfec1451b574f2353e7531fea76c2701
SukkaW <isukkaw@gmail.com>
2024-03-01 11:04:22 +0800

    Remove type assertion

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
+import { isSupportedPlatform } from 'utils';
 import { PLATFORM } from './const';
 import { Make } from './lib';
 
 (async () => {
     const [platform] = process.argv.slice(2);
 
-    if (!Object.values(PLATFORM).includes(platform as PLATFORM)) {
+    if (!isSupportedPlatform(platform)) {
         console.error(`${platform} not support, need ${Object.keys(PLATFORM)}`);
         throw new Error('not support');
     }
-    const m = new Make(platform as PLATFORM);
+    const m = new Make(platform);
     await m.makeManifest();
     m.copyStatic();
     m.makeRules();
--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,15 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
-import { OUT_DIR } from './const';
+import { PLATFORM } from './const';
 export function w(f: string, data: object) {
     fs.writeFileSync(f, JSON.stringify(data, null, 4), 'utf-8');
 }
 
 export function readSrcJson(f: string) {
     return JSON.parse(fs.readFileSync(path.join(__dirname, f), 'utf-8'));
+}
+
+export function isSupportedPlatform(platform: string): platform is PLATFORM {
+    return Object.values(PLATFORM).includes(platform as any);
 }
